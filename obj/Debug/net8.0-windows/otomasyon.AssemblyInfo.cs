ef1cae83100e0260d464ab68483f507a74f58c07
ercinAkkaya <ercinakkaya@gmail.com>
2026-07-13 11:54:52 +0300

    last commit

--- obj/Debug/net8.0-windows/otomasyon.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/otomasyon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("otomasyon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d314dec467f8dba5ad73267a4f72c8e68dfd473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd653fae35554a706a9c55c6fcd6690e965f7fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("otomasyon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("otomasyon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
